a0790d6d96368b99b78ebfc453aea2f86a53f312
Pierre Rudloff <contact@rudloff.pro>
2016-07-29 00:48:18 +0200

    Tests

---
+++ tests/linkifyTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Smarty_Modifier_linkify\Test;
+
+class LinkifyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLinkifyModifier()
+    {
+        $this->assertEquals(
+            'coucou',
+            Smarty_Modifier_linkify('coucou')
+        );
+        $this->assertEquals(
+            'Link to my website: <a href="https://rudloff.pro/">https://rudloff.pro/</a>',
+            Smarty_Modifier_linkify('Link to my website: https://rudloff.pro/')
+        );
+        $this->assertEquals(
+            'Contact me: <a href="mailto:contact@rudloff.pro">contact@rudloff.pro</a>',
+            Smarty_Modifier_linkify('Contact me: contact@rudloff.pro')
+        );
+    }
+}
